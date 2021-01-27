7c5e885feaa63c8d32bbdbbd280bbeb3168d278c
Nurul Hidayat <crmspy@gmail.com>
2021-01-27 16:22:02 +0700

    - add widget keybboard 70%

--- lib/ui/login/verify_otp.dart
+++ lib/ui/login/verify_otp.dart
@@ -176,12 +176,235 @@ class _VerifyOtpScreenState extends State<VerifyOtpScreen> {
                                       new BorderRadius.circular(10.0))),
                         ),
                       ),
+                      //pin box
+
+                      KeyboardNumber(onComplete: (data){ print("hit api verify"+data); },)
                     ],
                   ),
                 ),
               ],
             ),
           ),
+        ],
+      ),
+    );
+  }
+}
+
+class KeyboardNumber extends StatefulWidget {
+  final onComplete;
+  const KeyboardNumber({Key key,this.onComplete}) : super(key: key);
+
+  @override
+  _KeyboardNumberState createState() => _KeyboardNumberState(onComplete: this.onComplete);
+}
+
+class _KeyboardNumberState extends State<KeyboardNumber> {
+  final onComplete;
+  var arr = new List(6);
+  int activeBox = 0;
+  _KeyboardNumberState({this.onComplete});
+  void setState(fn) {
+    // TODO: implement setState
+    super.setState(fn);
+  }
+
+  void _handleClickNumber(int number){
+    int before = activeBox;
+    setState(() {
+      if (activeBox<6 && arr[activeBox] == null){
+        arr[activeBox]=number;
+      }
+      if (activeBox<6) activeBox++;
+      }
+    );
+    if (activeBox==before) { widget.onComplete(arr.join());};
+
+  }
+  void _handleClickBackspace(){
+    setState(() {
+      if (activeBox>0){
+
+        if (activeBox<=6){
+          activeBox--;
+          arr[activeBox]=null;
+        }else{
+          if (activeBox>1 || activeBox < 6)  activeBox = activeBox-1;
+        }
+      }
+    });
+  }
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          padding: EdgeInsets.all(5),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              BoxInputPin(isActive: arr[0] !=null ? true : false),
+              BoxInputPin(isActive: arr[1] !=null ? true : false),
+              BoxInputPin(isActive: arr[2] !=null ? true : false),
+              BoxInputPin(isActive: arr[3] !=null ? true : false),
+              BoxInputPin(isActive: arr[4] !=null ? true : false),
+              BoxInputPin(isActive: arr[5] !=null ? true : false),
+            ],
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  BoxNumber(number: 1, onClick: (){_handleClickNumber(1);}),
+                  BoxNumber(number: 2, onClick: (){_handleClickNumber(2);}),
+                  BoxNumber(number: 3, onClick: (){_handleClickNumber(3);}),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  BoxNumber(number: 4,onClick: (){_handleClickNumber(4);}),
+                  BoxNumber(number: 5,onClick: (){_handleClickNumber(5);}),
+                  BoxNumber(number: 6,onClick: (){_handleClickNumber(6);}),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  BoxNumber(number: 7,onClick: (){_handleClickNumber(7);}),
+                  BoxNumber(number: 8,onClick: (){_handleClickNumber(8);}),
+                  BoxNumber(number: 9,onClick: (){_handleClickNumber(9);}),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                      margin: EdgeInsets.all(10),
+                    height: 70,
+                    width: 70),
+                  BoxNumber(number: 0, onClick: (){_handleClickNumber(0);}),
+                  BoxNumber(icon: Icons.backspace_outlined,onClick: (){_handleClickBackspace();},),
+                ],
+              ),
+            ],
+          ),
+
+        ),
+      ],
+    ); //botton box
+  }
+}
+class BoxNumber extends StatelessWidget {
+  final number;
+  final icon;
+  final onClick;
+  const BoxNumber({Key key, this.number, this.icon,this.onClick}) : super(key: key);
+  @override
+  Widget IconBox(){
+    return Container(
+      height: 70,
+      width: 70,
+      margin: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          shape: BoxShape.rectangle,
+          borderRadius: new BorderRadius.circular(10.0)
+      ),
+      alignment: Alignment.center,
+      child: Container(
+        alignment: Alignment.center,
+        child: Icon(
+          this.icon,
+          color: AppColors.red,
+          size: 30.0,
+        ),
+      ),
+    );
+  }
+
+  Widget NumerBox(){
+    return Container(
+      height: 70,
+      width: 70,
+      margin: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          shape: BoxShape.rectangle,
+          borderRadius: new BorderRadius.circular(10.0)
+      ),
+      alignment: Alignment.center,
+      child: Container(
+        alignment: Alignment.center,
+        child: Text(this.number.toString(),style: TextStyle(
+          fontFamily: "roboto",
+          color: Colors.black,
+          fontSize: 26,
+          fontWeight: FontWeight.w600,
+        ),textAlign: TextAlign.center,),
+      ),
+    );
+  }
+
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onClick,
+      child: this.icon!=null ? IconBox() : NumerBox()
+    );
+  }
+}
+class BoxInputPin extends StatelessWidget {
+  final isActive;
+  const BoxInputPin({Key key, this.isActive}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(5),
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Container(
+            height: 60,
+            width: 50,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                shape: BoxShape.rectangle,
+                borderRadius: new BorderRadius.circular(15.0)
+            ),
+            alignment: Alignment.center,
+            padding: EdgeInsets.only(top: 20),
+          ),
+          isActive ?
+          Container(
+            height: 20,
+            width: 20,
+            decoration: BoxDecoration(
+                color: AppColors.red,
+                shape: BoxShape.rectangle,
+                borderRadius: new BorderRadius.circular(15.0)
+            ),
+            alignment: Alignment.center,
+            padding: EdgeInsets.only(top: 20),
+          ) : Container(
+            height: 20,
+            width: 20,
+            decoration: BoxDecoration(
+                color: Colors.black12,
+                shape: BoxShape.rectangle,
+                borderRadius: new BorderRadius.circular(15.0)
+            ),
+            alignment: Alignment.center,
+            padding: EdgeInsets.only(top: 20),
+          )
         ],
       ),
     );
