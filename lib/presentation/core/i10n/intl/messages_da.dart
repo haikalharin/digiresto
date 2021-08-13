44bda9d26c2fd0fb5b77e456b365446acf816136
Wibowo Raditya <wibowo@code.id>
2021-08-13 16:55:04 +0700

    update intl

---
+++ lib/presentation/core/i10n/intl/messages_da.dart
@@ -0,0 +1,48 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a da locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'da';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "home_end": MessageLookupByLibrary.simpleMessage(
+            "-------------------------------------------------------------------------------------"),
+        "home_start": MessageLookupByLibrary.simpleMessage(
+            "Below are list of strings for home"),
+        "home_tv_choose_language":
+            MessageLookupByLibrary.simpleMessage("Vælg sprog"),
+        "home_tv_error": MessageLookupByLibrary.simpleMessage("Fejl"),
+        "home_tv_no_post_found":
+            MessageLookupByLibrary.simpleMessage("Ingen indlæg fundet"),
+        "home_tv_posts": MessageLookupByLibrary.simpleMessage("Indlæg"),
+        "login_btn_forgot_password":
+            MessageLookupByLibrary.simpleMessage("Glemt adgangskode?"),
+        "login_btn_sign_in": MessageLookupByLibrary.simpleMessage("Log ind"),
+        "login_end": MessageLookupByLibrary.simpleMessage(
+            "------------------------------------------------------------------------------------"),
+        "login_error_fill_fields": MessageLookupByLibrary.simpleMessage(
+            "Udfyld venligst alle felterne"),
+        "login_et_user_email":
+            MessageLookupByLibrary.simpleMessage("Brugernavn"),
+        "login_et_user_password":
+            MessageLookupByLibrary.simpleMessage("Adgangskode"),
+        "login_start": MessageLookupByLibrary.simpleMessage(
+            "Below are list of strings for login da ")
+      };
+}
