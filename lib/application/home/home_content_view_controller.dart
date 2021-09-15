a14f91d06238ed1202664bdc3bb894ab5f2ceafe
Adam <adam@code.id>
2021-09-15 15:48:57 +0700

    add refresh indicator on home view

--- lib/application/home/home_content_view_controller.dart
+++ lib/application/home/home_content_view_controller.dart
@@ -1,7 +1,9 @@
+import 'package:digiresto/application/home/home_user_bloc/home_user_bloc.dart';
 import 'package:digiresto/domain/entity/order/static_banner_model.dart';
 import 'package:digiresto/domain/entity/user/user_get_address_model.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
+import 'package:provider/provider.dart';
 
 class HomeContentViewController extends GetxController {
   RxBool loadingHistory = false.obs;
@@ -37,4 +39,17 @@ class HomeContentViewController extends GetxController {
   setCurrentPosition(Position value) => currentPosition.value = value;
   setStaticBanner(List<StaticBanner> value) => listStaticBanner.value = value;
   setCurrentLocation(UserAddress value) => currentLocation.value = value;
+
+  void getStaticBanner() {
+    Get.context!.read<HomeUserBloc>().add(HomeUserEvent.getStaticBanner());
+  }
+
+  void getActiveAddress() {
+    Get.context!.read<HomeUserBloc>().add(HomeUserEvent.getActiveAddress());
+  }
+
+  void getRefresh() {
+    getStaticBanner();
+    getActiveAddress();
+  }
 }
